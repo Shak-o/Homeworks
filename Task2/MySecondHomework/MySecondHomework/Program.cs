8503cb44868c2e2ecd583945092f26ee79e182dc
Shak-O <shako.turashvili123@gmail.com>
2024-10-12 11:06:21 +0400

    gitignore

--- Task2/MySecondHomework/MySecondHomework/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// მას შემდეგ რაც შეასრულებთ დავალებას და შეამოწმებთ რომ ყველაფერი მუშაობს დააკომიტეთ ცვლილებები და დაფუშეთ remote repository ზე.
-Console.WriteLine("All working well horrey");
