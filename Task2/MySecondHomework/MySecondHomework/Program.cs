b5a14d813301f03da1130fb773c86f1aa0ba57c8
Shak-O <shako.turashvili123@gmail.com>
2024-10-12 11:08:37 +0400

    second homework ready

---
+++ Task2/MySecondHomework/MySecondHomework/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("My homework is ready!");
+// როცა დავალებას დაასრულებთ დააკომიტეთ და დაფუშეთ github ზე.
